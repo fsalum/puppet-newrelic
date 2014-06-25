4588bb83e8eb90e2756d5ee9d87bd55d830eb36b
Felipe Salum <fsalum@gmail.com>
2014-06-25 11:39:49 -0300

    Refactor module to major upgrade 4.x

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,25 +12,25 @@
 #
 class newrelic::params {
 
-  case $::operatingsystem {
-    'centos', 'redhat', 'fedora', 'amazon': {
+  case $::osfamily {
+    'RedHat': {
       $newrelic_package_name  = 'newrelic-sysmond'
       $newrelic_service_name  = 'newrelic-sysmond'
       $newrelic_php_package   = 'newrelic-php5'
       $newrelic_php_service   = 'newrelic-daemon'
-      $newrelic_php_conf_dir  = '/etc/php.d'
+      $newrelic_php_conf_dir  = ['/etc/php.d']
       package { 'newrelic-repo-5-3.noarch':
         ensure   => present,
         source   => 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm',
         provider => rpm,
       }
     }
-    'ubuntu', 'debian': {
+    'Debian': {
       $newrelic_package_name  = 'newrelic-sysmond'
       $newrelic_service_name  = 'newrelic-sysmond'
       $newrelic_php_package   = 'newrelic-php5'
       $newrelic_php_service   = 'newrelic-daemon'
-      $newrelic_php_conf_dir  = '/etc/php5/conf.d'
+      $newrelic_php_conf_dir  = ['/etc/php5/conf.d']
       apt::source { 'newrelic':
         location    => 'http://apt.newrelic.com/debian/',
         repos       => 'newrelic non-free',
--- manifests/php.pp
+++ manifests/php.pp
@@ -10,35 +10,9 @@
 # [*newrelic_php_service_ensure*]
 #   Specify the Newrelic PHP service running state. Defaults to 'running'. Possible value is 'stopped'.
 #
-# [*newrelic_php_conf_appname*]
-#   Sets the name of the application as it will be seen in the New Relic UI
-#
-# [*newrelic_php_conf_enabled*]
-#   By default the New Relic PHP agent is enabled for all directories. To disable set it to '0'.
-#
-# [*newrelic_php_conf_transaction*]
-#   This traces all calls, not just those calls internally instrumented by New Relic. To disable set it to '0'.
-#
-# [*newrelic_php_conf_logfile*]
-#   This identifies the file name for logging messages.
-#
-# [*newrelic_php_conf_loglevel*]
-#   Sets the level of detail of messages sent to the log file. Possible values: error, warning, info, verbose, debug, verbosedebug.
-#
-# [*newrelic_php_conf_browser*]
-#   This enables auto-insertion of the JavaScript fragments for browser monitoring. To disable set it to '0'.
-#
-# [*newrelic_php_conf_dberrors*]
-#   It causes MySQL errors from all instrumented MySQL calls to be reported to New Relic. To enable set it to '1'.
-#
-# [*newrelic_php_conf_transactionrecordsql*]
-#   When recording transaction traces internally, the full SQL for slow SQL calls is recorded. Possible values: off, raw, obfuscated.
-#
-# [*newrelic_php_conf_captureparams*]
-#   This will enable the display of parameters passed to a PHP script via the URL. To enable set it to '1'.
-#
 # [*newrelic_daemon_cfgfile_ensure*]
-#   Specify if the Newrelic daemon cfg file state. Defaults to 'present'. Possible value is 'absent'.
+#   Specify the Newrelic daemon cfg file state. Change to absent for agent startup mode. Defaults to 'present'. Possible value is 'absent'.
+#
 # Actions:
 #
 # Requires:
@@ -50,41 +24,73 @@
 #      newrelic_license_key        => 'your license key here',
 #      newrelic_php_package_ensure => 'latest',
 #      newrelic_php_service_ensure => 'running',
-#      newrelic_php_conf_appname   => 'Your PHP Application',
+#      newrelic_ini_appname        => 'Your PHP Application',
 #    }
 #
+# If no parameters are set it will use the newrelic.ini defaults
+#
+# For detailed explanation about the parameters below see: https://docs.newrelic.com/docs/php/php-agent-phpini-settings
+#
 define newrelic::php (
-  $newrelic_license_key                   = '',
-  $newrelic_php_package_ensure            = 'present',
-  $newrelic_php_service_ensure            = 'running',
-  $newrelic_php_conf_appname              = 'Your PHP Application',
-  $newrelic_php_conf_enabled              = '1',
-  $newrelic_php_conf_transaction          = '1',
-  $newrelic_php_conf_logfile              = '/var/log/newrelic/php_agent.log',
-  $newrelic_php_conf_loglevel             = 'info',
-  $newrelic_php_conf_browser              = '1',
-  $newrelic_php_conf_dberrors             = '0',
-  $newrelic_php_conf_transactionrecordsql = 'off',
-  $newrelic_php_conf_captureparams        = '0',
-  $newrelic_php_conf_ignoredparams        = '',
-  $newrelic_daemon_cfgfile_ensure         = 'present',
-  $newrelic_daemon_pidfile                = '/var/run/newrelic-daemon.pid',
-  $newrelic_daemon_logfile                = '/var/log/newrelic/newrelic-daemon.log',
-  $newrelic_daemon_loglevel               = 'info',
-  $newrelic_daemon_port                   = '33142',
-  $newrelic_daemon_ssl                    = false,
-  $newrelic_daemon_proxy                  = '',
-  $newrelic_daemon_ssl_ca_bundle          = '',
-  $newrelic_daemon_ssl_ca_path            = '',
-  $newrelic_daemon_max_threads            = '8',
-  $newrelic_daemon_collector_host         = 'collector.newrelic.com',
+  $newrelic_php_package_ensure                           = 'present',
+  $newrelic_php_service_ensure                           = 'running',
+  $newrelic_php_conf_dir                                 = $newrelic::params::newrelic_php_conf_dir,
+  $newrelic_license_key                                  = undef,
+  $newrelic_ini_appname                                  = undef,
+  $newrelic_ini_browser_monitoring_auto_instrument       = undef,
+  $newrelic_ini_enabled                                  = undef,
+  $newrelic_ini_error_collector_enabled                  = undef,
+  $newrelic_ini_error_collector_prioritize_api_errors    = undef,
+  $newrelic_ini_error_collector_record_database_errors   = undef,
+  $newrelic_ini_framework                                = undef,
+  $newrelic_ini_high_security                            = undef,
+  $newrelic_ini_logfile                                  = undef,
+  $newrelic_ini_loglevel                                 = undef,
+  $newrelic_ini_transaction_tracer_custom                = undef,
+  $newrelic_ini_transaction_tracer_detail                = undef,
+  $newrelic_ini_transaction_tracer_enabled               = undef,
+  $newrelic_ini_transaction_tracer_explain_enabled       = undef,
+  $newrelic_ini_transaction_tracer_explain_threshold     = undef,
+  $newrelic_ini_transaction_tracer_record_sql            = undef,
+  $newrelic_ini_transaction_tracer_slow_sql              = undef,
+  $newrelic_ini_transaction_tracer_stack_trace_threshold = undef,
+  $newrelic_ini_transaction_tracer_threshold             = undef,
+  $newrelic_ini_capture_params                           = undef,
+  $newrelic_ini_ignored_params                           = undef,
+  $newrelic_daemon_cfgfile_ensure                        = 'present',
+  $newrelic_daemon_dont_launch                           = undef,
+  $newrelic_daemon_pidfile                               = undef,
+  $newrelic_daemon_location                              = undef,
+  $newrelic_daemon_logfile                               = undef,
+  $newrelic_daemon_loglevel                              = undef,
+  $newrelic_daemon_port                                  = undef,
+  $newrelic_daemon_ssl                                   = undef,
+  $newrelic_daemon_ssl_ca_bundle                         = undef,
+  $newrelic_daemon_ssl_ca_path                           = undef,
+  $newrelic_daemon_proxy                                 = undef,
+  $newrelic_daemon_collector_host                        = undef,
+  $newrelic_daemon_auditlog                              = undef,
+  ### Deprecated below
+  $newrelic_php_conf_appname              = undef,
+  $newrelic_php_conf_enabled              = undef,
+  $newrelic_php_conf_transaction          = undef,
+  $newrelic_php_conf_logfile              = undef,
+  $newrelic_php_conf_loglevel             = undef,
+  $newrelic_php_conf_browser              = undef,
+  $newrelic_php_conf_dberrors             = undef,
+  $newrelic_php_conf_transactionrecordsql = undef,
+  $newrelic_php_conf_captureparams        = undef,
+  $newrelic_php_conf_ignoredparams        = undef,
 ) {
 
   include newrelic
 
   $newrelic_php_package  = $newrelic::params::newrelic_php_package
   $newrelic_php_service  = $newrelic::params::newrelic_php_service
-  $newrelic_php_conf_dir = $newrelic::params::newrelic_php_conf_dir
+
+  if ! $newrelic_license_key {
+    fail('You must specify a valid License Key.')
+  }
 
   package { $newrelic_php_package:
     ensure  => $newrelic_php_package_ensure,
@@ -96,32 +102,53 @@ define newrelic::php (
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    require    => Exec['/usr/bin/newrelic-install'],
   }
 
-  exec { '/usr/bin/newrelic-install':
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    command     => "/usr/bin/newrelic-install purge ; NR_INSTALL_SILENT=yes, NR_INSTALL_KEY=${newrelic_license_key} /usr/bin/newrelic-install install",
-    provider    => 'shell',
-    user        => 'root',
-    group       => 'root',
-    unless      => "cat ${newrelic_php_conf_dir}/newrelic.ini | grep ${newrelic_license_key}",
-    require     => Package[$newrelic_php_package],
-  }
-
-  file { 'newrelic.ini':
-    path    => "${newrelic_php_conf_dir}/newrelic.ini",
-    content => template('newrelic/newrelic.ini.erb'),
-    require => Exec['/usr/bin/newrelic-install'],
+  ::newrelic::php::newrelic_ini { $newrelic_php_conf_dir:
+    newrelic_license_key => $newrelic_license_key,
+    before               => [ File['/etc/newrelic/newrelic.cfg'], Service[$newrelic_php_service] ],
+    require              => Package[$newrelic_php_package],
+    notify               => Service[$newrelic_php_service],
   }
 
   file { '/etc/newrelic/newrelic.cfg':
     ensure  => $newrelic_daemon_cfgfile_ensure,
     path    => '/etc/newrelic/newrelic.cfg',
     content => template('newrelic/newrelic.cfg.erb'),
-    require => Exec['/usr/bin/newrelic-install'],
     before  => Service[$newrelic_php_service],
     notify  => Service[$newrelic_php_service],
+  }
+
+  # Fail on renamed/deprecated variables if they are still used
+  if $newrelic_php_conf_appname {
+    fail('Variable $newrelic_php_conf_appname is deprecated, use $newrelic_ini_appname instead.')
+  }
+  if $newrelic_php_conf_browser {
+    fail('Variable $newrelic_php_conf_browser is deprecated, use $newrelic_ini_browser_monitoring_auto_instrument instead.')
+  }
+  if $newrelic_php_conf_captureparams {
+    fail('Variable $newrelic_php_conf_captureparams is deprecated, use $newrelic_ini_capture_params instead.')
+  }
+  if $newrelic_php_conf_dberrors {
+    fail('Variable $newrelic_php_conf_dberrors is deprecated, use $newrelic_ini_error_collector_record_database_errors instead.')
+  }
+  if $newrelic_php_conf_enabled {
+    fail('Variable $newrelic_php_conf_enabled is deprecated, use $newrelic_ini_enabled instead.')
+  }
+  if $newrelic_php_conf_ignoredparams {
+    fail('Variable $newrelic_php_conf_ignoredparams is deprecated, use $newrelic_ini_ignored_params instead.')
+  }
+  if $newrelic_php_conf_logfile {
+    fail('Variable $newrelic_php_conf_logfile is deprecated, use $newrelic_ini_logfile instead.')
+  }
+  if $newrelic_php_conf_loglevel {
+    fail('Variable $newrelic_php_conf_loglevel is deprecated, use $newrelic_ini_loglevel instead.')
+  }
+  if $newrelic_php_conf_transactionrecordsql {
+    fail('Variable $newrelic_php_conf_transactionrecordsql is deprecated, use $newrelic_ini_transaction_tracer_record_sql instead.')
+  }
+  if $newrelic_php_conf_transaction {
+    fail('Variable $newrelic_php_conf_transaction is deprecated, use $newrelic_ini_transaction_tracer_detail instead.')
   }
 
 }
