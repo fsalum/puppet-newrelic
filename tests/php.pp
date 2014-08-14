8851985469fc5b77bc92fed51c89ca9b6ec8b741
Felipe Salum <fsalum@gmail.com>
2014-08-13 21:43:37 -0700

    fixing php conf dir for Debian/Ubuntu new PHP release

--- tests/php.pp
+++ tests/php.pp
@@ -8,13 +8,13 @@ node default {
   class { '::apache::mod::php': }
 
   class {'newrelic::server::linux':
-      newrelic_license_key => '',
+    newrelic_license_key => '',
   }
 
   class {'newrelic::agent::php':
-      newrelic_license_key                                => '',
-      #newrelic_php_conf_dir                               => ['/etc/php5/apache2/conf.d','/etc/php5/fpm/conf.d'],
-      require                                             => Class['Apache::mod::php'],
+    newrelic_license_key   => '',
+    #newrelic_php_conf_dir => ['/etc/php5/apache2/conf.d','/etc/php5/fpm/conf.d'],
+    require                => Class['Apache::mod::php'],
   }
 
 }
