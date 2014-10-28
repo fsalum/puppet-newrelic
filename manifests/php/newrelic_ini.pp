0f723ad1438a8482ca85073d7febc9f71512bb10
Chris Everest <ceverest@covermymeds.com>
2014-10-28 17:34:37 -0400

    Default to using the facter value of $::path
     - Convenient for non-standard php installations
     - Allows override if php isn't in the path by defalt

--- manifests/php/newrelic_ini.pp
+++ manifests/php/newrelic_ini.pp
@@ -1,10 +1,11 @@
 # This module should not be used directly. It is used by newrelic::php.
 define newrelic::php::newrelic_ini (
-  $newrelic_license_key
+  $newrelic_license_key,
+  $exec_path,
 ) {
 
   exec { "/usr/bin/newrelic-install ${name}":
-    path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    path     => $exec_path,
     command  => "/usr/bin/newrelic-install purge ; NR_INSTALL_SILENT=yes, NR_INSTALL_KEY=${newrelic_license_key} /usr/bin/newrelic-install install",
     provider => 'shell',
     user     => 'root',
