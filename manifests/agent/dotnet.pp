dea346d76b523946f2a5e9321811c271bda3a146
jweakley <j.weakley@nib.com.au>
2015-10-28 14:42:18 +1100

    Fix issues described in Travis BI build

--- manifests/agent/dotnet.pp
+++ manifests/agent/dotnet.pp
@@ -75,11 +75,11 @@ class newrelic::agent::dotnet (
   file { "${newrelic_dotnet_conf_dir}\\newrelic.config":
     ensure  => $newrelic_daemon_cfgfile_ensure,
     content => template('newrelic/newrelic.config.erb'),
-	notify  => Exec["iisreset"],
+    notify  => Exec["iisreset"],
   }
 
-  exec { "iisreset":
+  exec { 'iisreset':
     path        => 'C:/WINDOWS/System32',
-	refreshonly => true
+    refreshonly => true
   }
 }
