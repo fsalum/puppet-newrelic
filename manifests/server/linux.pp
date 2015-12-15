0ec49c18ea451ec2e9e4db98b2797b0bba095849
ehime <dodomeki@gmail.com>
2015-12-14 16:08:16 -0800

    Forgot manifests/server.pp

--- manifests/server/linux.pp
+++ manifests/server/linux.pp
@@ -51,7 +51,8 @@ class newrelic::server::linux (
   $newrelic_nrsysmond_collector_host = undef,
   $newrelic_nrsysmond_labels         = undef,
   $newrelic_nrsysmond_timeout        = undef,
-) inherits ::newrelic {
+  $newrelic_nrsysmond_disable_docker = undef,
+ ) inherits ::newrelic {
 
   if ! $newrelic_license_key {
     fail('You must specify a valid License Key.')
