2e9f6a0525c296aba9655d17f70662045aaedbdd
Peter H. Ruegg <peter.ruegg@vshn.net>
2017-03-27 17:20:26 +0200

    Make repository configurable

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,7 +10,9 @@
 #
 # Sample Usage:
 #
-class newrelic::params {
+class newrelic::params (
+  $newrelic_handle_repo = true,
+) {
 
   case $::osfamily {
     'RedHat': {
@@ -30,17 +32,19 @@ class newrelic::params {
       $newrelic_service_name  = 'newrelic-sysmond'
       $newrelic_php_package   = 'newrelic-php5'
       $newrelic_php_service   = 'newrelic-daemon'
-      apt::source { 'newrelic':
-        location => 'http://apt.newrelic.com/debian/',
-        repos    => 'non-free',
-        key      => {
-          id         => 'B60A3EC9BC013B9C23790EC8B31B29E5548C16BF',
-          key_source => 'https://download.newrelic.com/548C16BF.gpg',
-        },
-        include  => {
-          src => false,
-        },
-        release  => 'newrelic',
+      if $newrelic_handle_repo {
+        apt::source { 'newrelic':
+          location => 'http://apt.newrelic.com/debian/',
+          repos    => 'non-free',
+          key      => {
+            id         => 'B60A3EC9BC013B9C23790EC8B31B29E5548C16BF',
+            key_source => 'https://download.newrelic.com/548C16BF.gpg',
+          },
+          include  => {
+            src => false,
+          },
+          release  => 'newrelic',
+        }
       }
       case $::operatingsystem {
         'Debian': {
